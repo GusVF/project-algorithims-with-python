dd347943e220e20fed120bf663e9aaa2e7ae783c
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-06-02 13:16:11 -0300

    feat:challenge-03-recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,20 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+def is_palindrome_recursive(
+        word: str, low_index: int = 0, high_index: int = None
+        ):
+    if len(word) == 0:
+        return False
+    if high_index is None:
+        high_index = len(word) - 1
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+word = "abba"
+result = is_palindrome_recursive(word)
+print(result)
