038d467b98e79e9fd92eb5fac6f035d2c12c2b24
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-06-02 10:27:21 -0300

    feat:challange:test_encrypt.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,18 +1,21 @@
 permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
 target_time = 4
 
-# Se o numero for um horario nulo ou invalido retornar nulo
+# criar uma constante para armazenar o resultado
 # percorrer a lista
-# verificar quantas vezes o target time aparece na lista
+# verificar se os itens da lista e 'target_time' sao 'integers'
+# verificar quantas vezes o target_time aparece entre start_time/end_time
 # o numero que aparece mais vezes e o melhor horario.
 
 
 def study_schedule(permanence_period: list[tuple], target_time: int):
     best_time = 0
     for start_time, end_time in permanence_period:
-        if not (isinstance(start_time, int)
-                and isinstance(end_time, int)
-                and isinstance(target_time, int)):
+        if not (
+            isinstance(start_time, int)
+            and isinstance(end_time, int)
+            and isinstance(target_time, int)
+        ):
             return None
         if start_time <= target_time <= end_time:
             best_time += 1
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,34 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message("minimalist", 'string')
+
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(5, 3)
+
+    # Test case for key being out of range
+    result = encrypt_message("minimalist", 10)
+    assert result == "tsilaminim"
+
+    # Test case for key being within the range
+    result = encrypt_message("minimalist", 3)
+    assert result == "nim_tsilami"
+
+    # Test case for even key value
+    result = encrypt_message("minimalist", 6)
+    assert result == "tsil_aminim"
+
+    # Test case for empty message
+    result = encrypt_message("", 3)
+    assert result == ""
+
+    # Test case for key equal to message length
+    result = encrypt_message("minimalist", 10)
+    assert result == "tsilaminim"
+
+    # Test case for key equal to 1
+    result = encrypt_message("minimalist", 1)
+    assert result == "m_tsilamini"
